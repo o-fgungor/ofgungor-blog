d2e8e1ab9813a5ea21ea3a5f5c3498ff2b14a838
Omer Faruk Gungor <o.fgungor@outlook.com>
2025-05-10 02:26:52 +0200

    refactor: reset project

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia sit quos
+        illum quaerat quas, veritatis sequi eaque recusandae inventore nisi,
+        vitae deleniti nihil nesciunt quis corporis. Ea nostrum soluta cum
+        sapiente, libero debitis accusantium porro eligendi. Explicabo
+        distinctio deleniti earum asperiores ut? Labore exercitationem deserunt
+        aliquid natus ut quaerat commodi!
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
